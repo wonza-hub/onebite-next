a45168aac18e5a6ad7af6c9ffe1d0bdee3d34fc9
wonza-hub <wonza8585@gmail.com>
2025-04-15 18:26:43 +0900

    섹션 07 서버 액션 (~7.6)

--- section07/src/actions/create-review.action.ts
+++ section07/src/actions/create-review.action.ts
@@ -1,13 +1,18 @@
 "use server";
 
-export async function createReviewAction(formData: FormData) {
+import { revalidatePath, revalidateTag } from "next/cache";
+
+export async function createReviewAction(_: any, formData: FormData) {
   // File 타입은 받지 않으므로 null이 아니면 문자열로 변환
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요.",
+    };
   }
 
   try {
@@ -21,12 +26,29 @@ export async function createReviewAction(formData: FormData) {
         },
       }
     );
-    console.log(response.status);
+
     if (!response.ok) {
-      throw new Error("리뷰 작성에 실패했습니다.");
+      throw new Error(response.statusText);
     }
+    // // 1. 특정 주소에 해당하는 페이지만 재검증
+    // revalidatePath(`/book/${bookId}`);
+    // // 2. 특정 경로의 모든 동적 페이지를 재검증
+    // revalidatePath("/book/[id]", "page");
+    // // 3. 특정 레이아웃을 갖는 모든 페이지를 재검증
+    // revalidatePath("/(with-searchbar)", "layout");
+    // // 4. 모든 데이터 재검증
+    // revalidatePath("/", "layout");
+
+    // 5. 태그 기준, 데이터 캐시 재검증
+    revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    return {
+      status: false,
+      error: `리뷰 작성에 실패했습니다. ${err}`,
+    };
   }
 }
