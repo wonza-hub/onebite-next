5555d6b6fb3672e72a32c39e9f90d4f8a98b5829
wonza-hub <wonza8585@gmail.com>
2025-12-10 18:18:16 +0900

    feat: pre-fetching 실습 추가

--- pages-router/src/pages/_app.tsx
+++ pages-router/src/pages/_app.tsx
@@ -2,18 +2,25 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const onClickButton = () => {
-    router.push("/search");
+    router.push("/test");
   };
+
+  // PRE-FETCHING:프로그래밍 방식으로 페이지 이동 시 미리 페이지를 로드하기 위해 사용
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
 
   return (
     <>
       <header>
         <Link href={"/"}>index</Link>
         &nbsp;
+        {/* PRE-FETCHING: Link는 기본적으로 수행 */}
         <Link href={"/search"}>search</Link>
         &nbsp;
         <Link href={"/book/1"}>book/1</Link>
